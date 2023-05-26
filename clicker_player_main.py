1b85d061e2d2fa72dabf0e71a5758be99c56e03d
Eugene-q <looka12345@yandex.ru>
2023-05-26 14:31:14 +0300

    add main GUI frames and headers for tracklist, file operations, service sighals and player sections

--- clicker_player_main.py
+++ clicker_player_main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 from pygame import mixer as mix
 import tkinter as tki
+from tkinter import ttk
 
 PLAYBACK_DIR = 'music/'
 
@@ -69,22 +70,6 @@ class ClickPlayer:
             self.play_pause()
         else:
             print('FIRST TRACK !')
-
-def ask_user(message, options):
-    options_str = {}
-    allowed_nums = []
-    for num, option in enumerate(options):
-        options_str[str(num + 1).center(len(option))] = option
-        allowed_nums.append(str(num + 1))
-    while True:
-        print(message)
-        print(*options_str.values())
-        print(*options_str.keys())
-        choice = input()
-        if choice in allowed_nums:
-            break
-        print('Неправильно!')
-    return int(choice) - 1
     
 def turn_off(event=None):
     mix.music.stop()
@@ -93,18 +78,77 @@ def turn_off(event=None):
     
     
 player = ClickPlayer(PLAYBACK_DIR)
-choice = 100
-#controls = ('exit', 'play next', 'play/pause', 'vol+', 'vol-', 'previous and stop')
-#actions = (turn_off, player.play_next, player.play_pause, player.vol_up, player.vol_down, player.previous)
 
 window = tki.Tk()
-window.title('alien messenger')
-window.geometry('+400+50')
-window.minsize(width=400, height=700)
+window.title('Clicker Player')
+#window.geometry('+40+50')
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window_width = 700
+window_width = 700
+window.minsize(width=window_width, height=window_width)
+
+#initial gui elements
+
+frame_track_list = ttk.Frame(window, 
+                            relief=tki.GROOVE,
+                            borderwidth=4,
+                            padding=10,
+                            width=int(window_width / 3 * 2),
+                            )
+label_file_list_header = ttk.Label(frame_track_list, text='СПИСОК ТРЕКОВ')
+
+frame_add_tracks = ttk.Frame(window, 
+                            relief=tki.GROOVE,
+                            borderwidth=4,
+                            padding=10,
+                            width=int(window_width / 3),
+                            )
+label_add_tracks_header = ttk.Label(frame_add_tracks, text='ДОБАВИТЬ ТРЕКИ')
+scrollbar_list = tki.Scrollbar(frame_track_list)
+txt_tracks = tki.Text(frame_track_list, 
+                        width=70,
+                        height=40,
+                        wrap='word',
+                        yscrollcommand=scrollbar_list.set,
+                        )
+scrollbar_list.config(command=txt_tracks.yview)
+
+frame_service_signals = ttk.Frame(window, 
+                            relief=tki.GROOVE,
+                            borderwidth=4,
+                            padding=10,
+                            width=int(window_width / 3),
+                            )
+label_service_signals_header = ttk.Label(frame_service_signals, text='ЗВУКОВЫЕ СИГНАЛЫ')
+
+frame_player = ttk.Frame(window, 
+                            relief=tki.GROOVE,
+                            borderwidth=4,
+                            padding=10,
+                            width=int(window_width / 3),
+                            )
+label_player_header = ttk.Label(frame_player, text='ВОСПРОИЗВЕДЕНИЕ')
+
+#placement
+
+frame_track_list.pack(side='left', fill='y')
+label_file_list_header.pack(side='top')
+scrollbar_list.pack(side="right", fill="y") 
+txt_tracks.pack(side='left', fill='both')
+
+frame_add_tracks.pack(side='top', fill='both')
+label_add_tracks_header.pack(side='top')
+
+frame_service_signals.pack(fill='both')
+label_service_signals_header.pack(side='top')
+
+frame_player.pack(side='bottom', fill='both')
+label_player_header.pack(side='top')
 
 controls = {'0': turn_off,
             '<Escape>': player.play_next,
-            #'<Shift_L>': player.play_next,
+            '<Shift_L>': player.play_next,
             '<Tab>': player.play_pause,
             '<Up>': player.vol_up, 
             '<Down>': player.vol_down,
@@ -114,22 +158,3 @@ for key, action_func in controls.items():
     window.bind(key, action_func)
     
 window.mainloop()
-
-#while choice:
-    # for event in pygame.event.get():
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_1:
-#                 choice = 1
-#             elif event.key == pygame.K_2:
-#                 choice = 2
-#             elif event.key == pygame.K_3:
-#                 choice = 3
-#             elif event.key == pygame.K_4:
-#                 choice = 4
-#             elif event.key == pygame.K_5:
-#                 choice = 5
-#             elif event.key == pygame.K_0:
-#                 choice = 0
-    #choice = ask_user('input controls', controls)
-    #actions[choice]()
-    
