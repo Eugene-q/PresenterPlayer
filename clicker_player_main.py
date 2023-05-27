85e7830b2ec7ed909f2ce41dd4a9821dada38cc4
Eugene-q <looka12345@yandex.ru>
2023-05-28 01:34:21 +0300

    add buttons in -list_operations- section, -signals- section(buttons and scale for setting signals vol) and in -track_list- section button for add new files. All buttons are not working for a while

--- clicker_player_main.py
+++ clicker_player_main.py
@@ -88,23 +88,28 @@ window_width = 700
 window_width = 700
 window.minsize(width=window_width, height=window_width)
 
+HORIZ_VOL_IMAGE = tki.PhotoImage(file='assets/images/horiz_vol.png',
+                                 width=100,
+                                 height=20,
+                                 )
+
 #initial gui elements
 
 frame_track_list = ttk.Frame(window, 
                             relief=tki.GROOVE,
                             borderwidth=4,
-                            padding=10,
+                            padding=5,
                             width=int(window_width / 3 * 2),
                             )
-label_file_list_header = ttk.Label(frame_track_list, text='СПИСОК ТРЕКОВ')
-
-frame_add_tracks = ttk.Frame(window, 
-                            relief=tki.GROOVE,
-                            borderwidth=4,
-                            padding=10,
-                            width=int(window_width / 3),
-                            )
-label_add_tracks_header = ttk.Label(frame_add_tracks, text='ДОБАВИТЬ ТРЕКИ')
+frame_add_tracks = ttk.Frame(frame_track_list)
+label_file_list_header = ttk.Label(frame_add_tracks,
+                                   text='СПИСОК ЗВУКОЗАПИСЕЙ',
+                                   justify=tki.CENTER,
+                                   )
+button_add_tracks = ttk.Button(master=frame_add_tracks,
+                        text='Добавить',
+                        padding=3,
+                        )
 scrollbar_list = tki.Scrollbar(frame_track_list)
 txt_tracks = tki.Text(frame_track_list, 
                         width=70,
@@ -114,37 +119,89 @@ txt_tracks = tki.Text(frame_track_list,
                         )
 scrollbar_list.config(command=txt_tracks.yview)
 
+frame_list_operations = ttk.Frame(window, 
+                            relief=tki.GROOVE,
+                            borderwidth=4,
+                            padding=5,
+                            width=int(window_width / 3),
+                            )
+label_add_tracks_header = ttk.Label(frame_list_operations, text='УПРАВЛЕНИЕ СПИСКОМ')
+button_load = ttk.Button(master=frame_list_operations,
+                        text='Загрузить',
+                        padding=3,
+                        )
+button_save = ttk.Button(master=frame_list_operations,
+                        text='Сохранить',
+                        padding=3,
+                        )
+button_clear = ttk.Button(master=frame_list_operations,
+                        text='Очистить',
+                        padding=3,
+                        )
+
 frame_service_signals = ttk.Frame(window, 
                             relief=tki.GROOVE,
                             borderwidth=4,
-                            padding=10,
+                            padding=5,
                             width=int(window_width / 3),
                             )
-label_service_signals_header = ttk.Label(frame_service_signals, text='ЗВУКОВЫЕ СИГНАЛЫ')
+label_service_signals_header = ttk.Label(frame_service_signals, text='СИГНАЛЫ')
+button_signal_type = ttk.Button(master=frame_service_signals,
+                                text='Тип\nсигнала',
+                                padding=3,
+                                )
+frame_signal_volume = ttk.Frame(frame_service_signals)
+label_signal_volume = ttk.Label(frame_signal_volume,
+                            image=HORIZ_VOL_IMAGE,
+                            )
+scale_signal_volume = ttk.Scale(master=frame_signal_volume,
+                                orient=tki.HORIZONTAL,
+                                length=100,
+                                from_=0,
+                                to=100,
+                                value=50,
+                                )
+button_signals_off = ttk.Button(master=frame_service_signals,
+                                text='Отключить\nсигналы',
+                                padding=3,
+                                )
+#print(button_signals_off.configure().keys())                
 
 frame_player = ttk.Frame(window, 
                             relief=tki.GROOVE,
                             borderwidth=4,
-                            padding=10,
+                            padding=5,
                             width=int(window_width / 3),
                             )
 label_player_header = ttk.Label(frame_player, text='ВОСПРОИЗВЕДЕНИЕ')
 
 #placement
 
-frame_track_list.pack(side='left', fill='y')
-label_file_list_header.pack(side='top')
-scrollbar_list.pack(side="right", fill="y") 
+frame_track_list.pack(side='left', fill='both')
+frame_add_tracks.pack(fill='x')
+label_file_list_header.pack(side='left', fill='x')
+button_add_tracks.pack(side='right')
+scrollbar_list.pack(side="right", fill='y') 
 txt_tracks.pack(side='left', fill='both')
 
-frame_add_tracks.pack(side='top', fill='both')
+frame_list_operations.pack(side='top', fill='both')
 label_add_tracks_header.pack(side='top')
+button_load.pack(side='left', fill='y')
+button_save.pack(side='left')
+button_clear.pack(side='right', fill='y')
 
 frame_service_signals.pack(fill='both')
-label_service_signals_header.pack(side='top')
+label_service_signals_header.pack(side='top', pady=3)
+button_signal_type.pack(side='left')
+frame_signal_volume.pack(side='left')
+label_signal_volume.pack(fill='x')
+scale_signal_volume.pack(fill='x', padx=2,)
+button_signals_off.pack(side='left')
 
 frame_player.pack(side='bottom', fill='both')
 label_player_header.pack(side='top')
+
+frame_add_tracks.pack()
 
 controls = {'0': turn_off,
             '<Escape>': player.play_next,
